cdb859d776bdf9dcffbee4dc752e7becb7aa8829
NicoFJCruz <nicofjcruz@gmail.com>
2023-06-30 18:10:46 -0300

    Index.js base

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const db = require("./db");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const ORIGIN = process.env.ORIGIN || "http://localhost:5173";
+const corsOptions = {
+  credentials: true,
+  origin: ORIGIN,
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+};
+
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(express.json());
+//app.use("/api");
+
+db.sync({ force: false }).then(() => {
+  console.log("DB connected");
+
+  app.listen(PORT, () => {
+    console.log(`Server listening at port ${PORT}`);
+  });
+});
