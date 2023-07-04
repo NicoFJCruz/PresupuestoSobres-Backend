31272d42028937ce42c16971f2f9c0205394b619
NicoFJCruz <nicofjcruz@gmail.com>
2023-07-04 18:08:10 -0300

    Models Relations

--- models/index.js
+++ models/index.js
@@ -6,6 +6,30 @@ const accounts = require("./Accounts");
 const transactions = require("./Transactions");
 const entities = require("./Entity");
 
+//Relation Family - User
+families.hasMany(users)
+users.belongsTo(families)
+
+//Relation Family - Envelopes
+families.hasMany(envelopes)
+envelopes.belongsTo(families)
+
+//Relation Family - Accounts
+families.hasMany(accounts)
+accounts.belongsTo(families)
+
+//Relation Envelopes - Transactions
+envelopes.hasMany(transactions)
+transactions.belongsTo(envelopes)
+
+//Relation Accounts - Transactions
+accounts.hasMany(transactions)
+transactions.belongsTo(accounts)
+
+//Relation Entities - Transactions
+entities.hasMany(transactions)
+transactions.belongsTo(entities)
+
 module.exports = {
   users,
   families,
