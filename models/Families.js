fc300cdf6fe72b465c54b63e436528e77fac2ba4
NicoFJCruz <nicofjcruz@gmail.com>
2023-07-06 10:57:46 -0300

    Family model fix

--- models/Families.js
+++ models/Families.js
@@ -3,6 +3,9 @@ const db = require("../db");
 
 class Family extends S.Model {}
 
-Family.init({}, { sequelize: db, modelName: "families" });
+Family.init(
+  { name: { type: S.STRING, allowNull: false } },
+  { sequelize: db, modelName: "families" }
+);
 
 module.exports = Family;
